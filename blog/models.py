46604bc311320cbfb9e280974252d4f2d7dc5c6a
Mildness <m.akomoize@gmail.com>
2022-07-05 23:47:29 +0100

    Modified comments

--- blog/models.py
+++ blog/models.py
@@ -36,11 +36,9 @@ class Post(models.Model):
     # def get_absolute_url(self):
     #     return reverse("blog:post_detail", kwargs={"slug": self.slug})
     
-def get_absolute_url(self):
-    return reverse('blog:post_detail',
-    args=[self.publish.year,
-    self.publish.month,
-    self.publish.day, self.slug])
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+        args=[self.slug])
     
     
 class Comment(models.Model):
